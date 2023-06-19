37ec53691ba3a9f4dac8b5fb5a74a336ea7f0403
Maxim Grossu <maxgrossu@yandex.ru>
2023-06-19 18:38:22 +0300

    созданы модели card и user

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const express = require('express');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
+app.get('/', (req, res) => {
+  res.send('hello world2');
 });
+
+app.listen(PORT, () => {});
