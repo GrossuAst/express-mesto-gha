9df9193fc93ca054c2d02597b05886c64f2d5b11
Maxim Grossu <maxgrossu@yandex.ru>
2023-06-29 22:24:31 +0300

    добавлен мидлвэр авторизации

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
+
+const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 // импорт роутов
-const userRoutes = require('./routes/users');
-const cardRoutes = require('./routes/cards');
 const nonExistenRoutes = require('./routes/nonExistenRoutes');
 const registerRouter = require('./routes/register');
 const loginRouter = require('./routes/login');
+const auth = require('./middlewares/auth');
+const userRoutes = require('./routes/users');
+const cardRoutes = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 
@@ -15,21 +18,12 @@ mongoose.connect('mongodb://127.0.0.1/mestodb', {
   useNewUrlParser: true,
 });
 
-const app = express();
-
-// хардкод айди пользователя для карточки
-app.use((req, res, next) => {
-  req.user = {
-    _id: '6492008cc8bd209608b8ff59',
-  };
-  next();
-});
-
 app.use(bodyParser.json());
-app.use(userRoutes);
-app.use(cardRoutes);
 app.use(registerRouter);
 app.use(loginRouter);
+app.use(auth);
+app.use(userRoutes);
+app.use(cardRoutes);
 app.use(nonExistenRoutes);
 
 app.listen(PORT, () => {});
---
+++ middlewares/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { auth } = req.headers;
+  if (!auth || !auth.startsWith('Bearer ')) {
+    return res.status(401).send({ message: 'Необходима авторизация' });
+  }
+  const token = auth.replace('Bearer ');
+  // const payload = jwt.verify(token, 'key');
+  let payload;
+  try {
+    payload = jwt.verify(token, 'key');
+  } catch (err) {
+    return res.status(401).send({ message: 'Необходима авторизация' });
+  }
+  req.user = payload;
+  next();
+};
