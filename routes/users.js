2933d826a0c2338708c2b18e7a1e79728b7f9faf
Maxim Grossu <maxgrossu@yandex.ru>
2023-07-06 12:54:55 +0300

    test

--- routes/users.js
+++ routes/users.js
@@ -11,18 +11,22 @@ const {
 } = require('../controllers/users');
 
 router.get('/users', getAllUsers);
-router.post('/users/me', getInfoAboutMe);
+
+router.get('/users/me', getInfoAboutMe);
+
 router.get('/users/:id', celebrate({
   params: Joi.object().keys({
     id: Joi.string().length(24),
   }),
 }), getUserById);
+
 router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
 }), updateProfile);
+
 router.patch('/users/me/avatar', updateAvatar);
 
 module.exports = router;
